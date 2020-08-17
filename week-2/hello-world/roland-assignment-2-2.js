76067d93da05985046a7435d4432573cb36ce654
Jonathan Roland <jdroland31@yahoo.com>
2020-08-17 17:58:50 -0400

    correction for week 2 files place in wrong class directory

---
+++ week-2/hello-world/roland-assignment-2-2.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var http = require("http");
+
+var app = express();
+
+app.use(function(request,response){
+    console.log("in comes a request to: "+request.url);
+    response.end("Hello World\n");
+});
+
+http.createServer(app).listen(8080, function(){
+    console.log('Application started on port 8080');
+});
