21aa86a3369564cc4aaacf44a52e529734e91ed0
Jonathan Roland <jdroland31@yahoo.com>
2020-08-31 22:33:35 -0400

    added assignment 4.4

---
+++ week-4/crud/roland-assignment-4-4.js
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title:  roland-assignment-4-4.js
+; Author: Professor Krasso
+; Date:   31 August 2020
+; Description: Demonstrates CRUD operations in a
+;              Node.js API.
+;===========================================
+*/
+
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.get("/", function(request, response) {
+  response.send("API invoked as an HTTP GET request.");
+});
+
+app.put("/", function(request, response) {
+  response.send("API invoked as an HTTP PUT request.");
+});
+
+app.post("/", function(request, response) {
+  response.send("API invoked as an HTTP POST request");
+});
+
+app.delete("/", function(request, response) {
+  response.send("API invoked as an HTTP DELETE request");
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
