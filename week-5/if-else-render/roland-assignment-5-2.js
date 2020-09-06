df185b373f08d6408a05ca0c96dda403a4eeb2b9
Jonathan Roland <jdroland31@yahoo.com>
2020-09-06 00:29:21 -0400

    added comments to 5.2

--- week-5/if-else-render/roland-assignment-5-2.js
+++ week-5/if-else-render/roland-assignment-5-2.js
@@ -1,39 +1,29 @@
+//dependency setup
 var express = require("express");
-
 var http = require("http");
-
 var path = require("path");
-
 app = express();
-
 app.set("views", path.resolve(__dirname, "views"));
-
 app.set("view engine", "ejs");
 
+//create an array of names
 var f = [
-
   "Jake",
-
   "Lucy",
-
   "Sven",
-
   "Annette"
-
 ];
 
+//set the home route
 app.get("/", function(request, response) {
-
+    //render the index page
     response.render("index", {
-
+        //pass index.js a variable 'names' with the array of names
         names: f
-
     })
-
 });
 
+//create server on localhost port 8080
 http.createServer(app).listen(8080, function() {
-
     console.log("Application started on port 8080!");
-
 });
