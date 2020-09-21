367ca7cab509c93ee2b884bb086ed4ca4f770bd6
Jonathan Roland <jdroland31@yahoo.com>
2020-09-20 20:48:12 -0400

    added week 7 directory and assignment 7.2

---
+++ week-7/tdd-example/test/roland-assignment-7-2.js
@@ -0,0 +1,19 @@
+var assert = require("assert");
+
+describe("String#split", function() {
+    it("should return an array of fruits", function() {
+        assert(Array.isArray('Apple,Orange,Mango'.split(',')));
+    });
+});
+
+// Run the test
+
+//         npm test
+
+// Passed function
+
+function getFruits(str) {
+    return str.split(',');
+}
+
+module.exports = getFruits;
