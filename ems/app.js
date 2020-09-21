38c6b54576504f04abd6bbf6d491ff1a197372cf
Jonathan Roland <jdroland31@yahoo.com>
2020-09-20 22:55:12 -0400

    added employee.js schema

--- ems/app.js
+++ ems/app.js
@@ -1,12 +1,39 @@
 //require
 var express = require("express");
 var http = require("http");
+var mongoose = require("mongoose");
 var path = require("path");
 var logger = require("morgan");
 var app = express();
 
+var Employee = require("./models/employee");
 
+// Database Connection
+// mLab connection
 
+var mongoDB = "mongodb+srv://secondUser:VXYmsanInCprgvg0@buwebdev-cluster-1.esxqv.mongodb.net/test?authSource=admin&replicaSet=atlas-mshxui-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+db.once("open", function() {
+    console.log("Application connected to mLab MongoDB instance");
+});
+
+//add employee model
+var employee = new Employee({
+    firstName: "Jim",
+    lastName: "Smith"
+});
+
+//dependencies
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("short"));
---
+++ ems/models/employee.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title:  employee.js
+; Author: Professor Krasso
+; Modified By: Jonathan Roland
+; Date:   28 February 2019
+; Description: File for the Employee database model
+;===========================================
+*/
+
+// Require statements
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Employee Schema
+let EmployeeSchema = new Schema({
+  firstName: {type: String, required: true },
+  lastName: {type: String, required: true}
+});
+
+// Export the model so its publicly available.
+module.exports = mongoose.model('Employee', EmployeeSchema);
