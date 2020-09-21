dc7a31cf7f442af0cef55bdcfa5c1a858f02819e
Jonathan Roland <jdroland31@yahoo.com>
2020-09-20 21:15:49 -0400

    added assignment 7.3

---
+++ week-7/chai-example/test/roland-assignment-7-3.js
@@ -0,0 +1,17 @@
+var fruits = require("../roland-fruits");
+
+var chai = require("chai");
+
+var assert = chai.assert;
+
+describe("fruits", function() {
+
+    it("should return an array of fruits", function() {
+
+        var f = fruits('Apple,Orange,Mango');
+
+        assert(Array.isArray(f));
+
+    });
+
+});
